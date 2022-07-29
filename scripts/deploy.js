92d7e701f23a5982f8e4913083405a9c3406a06f
Drakenn04 <92858689+Drakenn04@users.noreply.github.com>
2022-07-29 18:28:36 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const {ethers} = require("hardhat");
+
+async function main(){
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    "SimpleStorage"
+  )
+  console.log("Deploying....");
+  const SimpleStorage = await SimpleStorageFactory.deploy();
+  await SimpleStorage.deployed();
+  console.log(`Deployed contract to: ${SimpleStorage.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
